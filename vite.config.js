363b5f6301faea0e6b809a07c3b92f9fd23bbf00
Fadi Ikkene <fadeta390@gmail.com>
2024-12-31 20:08:42 +0100

    prepare for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
-    resolve:{
-        alias:{
-            '@': path.resolve(__dirname,'./resources/js'),
-            '@Layouts': path.resolve(__dirname,'./resources/js/Layouts'),
-            '@Components': path.resolve(__dirname,'./resources/js/Components')
-        }
-    }
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/js"),
+            "@Layouts": path.resolve(__dirname, "./resources/js/Layouts"),
+            "@Components": path.resolve(__dirname, "./resources/js/Components"),
+        },
+    },
+    build: {
+        outDir: "public/build", 
+        emptyOutDir: true,
+    },
 });
